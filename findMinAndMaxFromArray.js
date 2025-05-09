57dd4a9c5876bd9c1e38585f3b3d3f471e187f00
Jeevan Kumar <114385490+jeevan42@users.noreply.github.com>
2025-05-09 23:32:23 +0530

    Create findMinAndMaxFromArray.js

---
+++ findMinAndMaxFromArray.js
@@ -0,0 +1,21 @@
+// Find Min and Max element in an array.
+
+
+let array = [8, 9, 51, 4, 15];
+
+
+const getMin = (arr) => {
+    return arr.reduce((pre, cur) => {
+        // console.log(`pre & cur`, { pre, cur });
+        return pre < cur ? pre : cur;
+    })
+}
+
+const getMax = (arr) => {
+    return arr.reduce((pre, cur) => {
+        return pre > cur ? pre : cur;
+    })
+}
+
+console.log(`Min is :`, getMin(array));
+console.log(`Max is :`, getMax(array));
