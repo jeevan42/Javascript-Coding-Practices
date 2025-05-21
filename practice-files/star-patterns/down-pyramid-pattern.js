375116b3b5a6da3fab9adbdaee3f03c5fb57c578
jeevan42 <g1kumar05092002@gmail.com>
2025-05-22 00:30:13 +0530

    downPyramidPattern.js added

---
+++ practice-files/star-patterns/down-pyramid-pattern.js
@@ -0,0 +1,14 @@
+const dowmPyramidPattern = (n) => {
+    for (let i = n; i >= 1; i--) {
+       let stars = "";
+       for (let j = 1; j <= n - i; j++) {
+        stars += "  ";
+       }
+       for (let k = 1; k <= 2 * i - 1; k++) {
+        stars += "* ";
+       }
+       console.log(stars)
+    }
+}
+
+dowmPyramidPattern(5);
