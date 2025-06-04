bde7629e9518f902deec39b1fde33c8f392e212c
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:47:30 +0530

    Create js-object-definition.js

---
+++ practice-files/js-object-definition.js
@@ -0,0 +1,12 @@
+/*
+📘 JavaScript Object - Definition:
+An object is a collection of key-value pairs. Values can be of any data type, including other objects or functions.
+
+const person = {
+  name: "Alice",
+  age: 25,
+  greet: function() { console.log("Hi"); }
+};
+
+Objects are used to group related data and functionality.
+*/
