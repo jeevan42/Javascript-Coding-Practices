eeae9038d3e637c24ff17e138094b238ea02d9c8
jeevan42 <g1kumar05092002@gmail.com>
2025-06-05 17:54:33 +0530

    event loop folder addition with js-eventloop-usecase.js

---
+++ event-loop/js-eventloop-usecase.js
@@ -0,0 +1,12 @@
+/*
+📘 Real Use Case:
+Why use event loop model?
+
+✅ Example: User clicks + data fetch
+- UI event added to macrotask queue
+- Fetch is handled by Web API
+- Response added to microtask queue
+- Fast UI responsiveness ensured
+
+📌 Asynchronous behavior prevents UI blocking.
+*/
