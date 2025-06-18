3316197a762cf6565d5acc501e06cee4a6092f02
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-18 18:21:02 +0530

    Create 17-instanceof-trick.js

---
+++ predictions-output/17-instanceof-trick.js
@@ -0,0 +1,10 @@
+// 🧠 instanceof with null and undefined
+
+console.log(null instanceof Object); 
+// ❌ false
+
+console.log(undefined instanceof Object); 
+// ❌ false
+
+console.log([] instanceof Array); 
+// ✅ true
