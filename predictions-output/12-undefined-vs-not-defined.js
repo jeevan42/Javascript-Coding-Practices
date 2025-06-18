9602c5827b974d78671185baefea0f50c5234bb6
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-18 18:18:47 +0530

    Create 12-undefined-vs-not-defined.js

---
+++ predictions-output/12-undefined-vs-not-defined.js
@@ -0,0 +1,10 @@
+// 🧠 undefined vs not defined
+
+let a;
+console.log(a); 
+// ✅ undefined
+// 🧠 Declared but not assigned → default value is undefined
+
+console.log(b); 
+// ❌ ReferenceError: b is not defined
+// 🧠 b was never declared → not even in memory
