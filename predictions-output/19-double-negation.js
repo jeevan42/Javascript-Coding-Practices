9141344f47032571ad49581d93c9a9f8ad6ae534
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-18 18:21:47 +0530

    Create 19-double-negation.js

---
+++ predictions-output/19-double-negation.js
@@ -0,0 +1,10 @@
+// 🧠 !! Double Negation
+
+console.log(!!"hello");  
+// ✅ true
+console.log(!!0);        
+// ✅ false
+console.log(!![]);       
+// ✅ true
+
+// 🧠 `!!` is used to cast any value into a boolean
