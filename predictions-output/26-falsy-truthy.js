96cfd4288db29adfc7f19428de6a7cf8a5f7844b
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-19 19:56:37 +0530

    Create 26-falsy-truthy.js

---
+++ predictions-output/26-falsy-truthy.js
@@ -0,0 +1,8 @@
+// 🧠 Falsy and Truthy Values
+
+console.log(Boolean([]));     // ✅ true
+console.log(Boolean({}));     // ✅ true
+console.log(Boolean(""));     // ✅ false
+console.log(Boolean(undefined)); // ✅ false
+
+// 🧠 [], {} are objects → always truthy in JS
