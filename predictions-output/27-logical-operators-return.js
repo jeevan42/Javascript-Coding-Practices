f2fd9760e2f2fcdd39c6f50e9e254d7d7bcdfa31
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-19 19:56:54 +0530

    Create 27-logical-operators-return.js

---
+++ predictions-output/27-logical-operators-return.js
@@ -0,0 +1,10 @@
+// 🧠 Logical Operators Return Value
+
+console.log(0 || "Hello"); 
+// ✅ Output: Hello
+console.log("Hi" && 42);   
+// ✅ Output: 42
+console.log(null && "Text"); 
+// ✅ Output: null
+
+// 🧠 || returns first truthy, && returns last if all truthy
