6bf1a1bb656049f1c8db3f7e39702ca9eb8ec729
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-21 14:26:03 +0530

    Create 68-void-operator.js

---
+++ predictions-output/68-void-operator.js
@@ -0,0 +1,6 @@
+// 🧠 void always returns undefined
+
+console.log(void 0);     // ✅ undefined
+console.log(void "text"); // ✅ undefined
+
+// 🧠 Often used in bookmarks or IIFEs to prevent unwanted returns
