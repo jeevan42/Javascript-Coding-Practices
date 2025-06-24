46f1b9474e8a226576e6a96da4df1d6e3c26d02e
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-24 08:55:21 +0530

    Create 94-date-now-vs-newdate.js

---
+++ predictions-output/94-date-now-vs-newdate.js
@@ -0,0 +1,6 @@
+// 🧠 Date.now() vs new Date()
+
+console.log(Date.now());      // ✅ timestamp (ms)
+console.log(new Date());      // ✅ full date string
+
+// 🧠 Date.now() gives current timestamp in ms
