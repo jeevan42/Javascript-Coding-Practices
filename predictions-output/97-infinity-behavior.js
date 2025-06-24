edff9d0bb7d61e4a792a80e8a87d3c7dc7d5cca8
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-24 08:56:57 +0530

    Create 97-infinity-behavior.js

---
+++ predictions-output/97-infinity-behavior.js
@@ -0,0 +1,7 @@
+// 🧠 Infinity quirks
+
+console.log(1 / 0);        // ✅ Infinity
+console.log(-1 / 0);       // ✅ -Infinity
+console.log(Infinity > 999999); // ✅ true
+
+// 🧠 Division by zero in JS doesn’t throw error
