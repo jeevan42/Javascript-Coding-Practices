ca00d848ddf94876ceaea3a8fefe04ae5571aa48
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-27 00:58:51 +0530

    Create 127-number-to-string-base.js

---
+++ predictions-output/127-number-to-string-base.js
@@ -0,0 +1,6 @@
+// 🧠 Convert number to binary, hex etc.
+
+console.log((255).toString(2));  // ✅ "11111111"
+console.log((255).toString(16)); // ✅ "ff"
+
+// 🧠 toString(radix) is powerful for base conversion
