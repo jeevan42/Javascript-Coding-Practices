d3b4aff77854d42c8845c625b4b3252babd0b58d
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-27 01:01:31 +0530

    Create 130-locale-compare.js

---
+++ predictions-output/130-locale-compare.js
@@ -0,0 +1,7 @@
+// 🧠 String localeCompare()
+
+console.log("a".localeCompare("b")); // ✅ -1
+console.log("b".localeCompare("a")); // ✅ 1
+console.log("a".localeCompare("a")); // ✅ 0
+
+// 🧠 Useful for alphabetical sorting with accents/locales
