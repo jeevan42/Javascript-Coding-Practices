5aeed31ddddac352ac29fd60dfe07f8d0611ffd8
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-27 00:54:47 +0530

    Create 122-string-pad-start.js

---
+++ predictions-output/122-string-pad-start.js
@@ -0,0 +1,6 @@
+// 🧠 .padStart() pads string from left
+
+console.log("5".padStart(3, "0")); // ✅ "005"
+console.log("abc".padStart(6, ".")); // ✅ "...abc"
+
+// 🧠 Useful for formatting numbers, times, etc.
