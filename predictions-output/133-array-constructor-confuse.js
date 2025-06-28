1f2f968b66f715d71f479277f6e910523ce100c3
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-28 12:52:26 +0530

    Create 133-array-constructor-confuse.js

---
+++ predictions-output/133-array-constructor-confuse.js
@@ -0,0 +1,6 @@
+// 🧠 Array constructor can be confusing
+
+console.log(new Array(3));   // ✅ [ <3 empty items> ]
+console.log([3]);            // ✅ [3]
+
+// 🧠 `new Array(3)` creates length, not values
