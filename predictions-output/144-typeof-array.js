577841bf2b3eafc596a5b11cafe31b096a79d9c0
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-29 10:06:30 +0530

    Create 144-typeof-array.js

---
+++ predictions-output/144-typeof-array.js
@@ -0,0 +1,6 @@
+// 🧠 typeof [] is 'object'
+
+console.log(typeof []);              // ✅ "object"
+console.log(Array.isArray([]));      // ✅ true
+
+// 🧠 typeof can't differentiate between objects and arrays
