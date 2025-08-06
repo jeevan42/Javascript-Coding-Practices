ebe110c47c2498a48ea0ed986ecde2498a3d6b0e
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-06 23:07:51 +0530

    Create 529-is-palindrome.js

---
+++ predictions-output/529-is-palindrome.js
@@ -0,0 +1,6 @@
+const isPalindrome = (str) => {
+  const cleaned = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  return cleaned === cleaned.split('').reverse().join('');
+};
+
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
