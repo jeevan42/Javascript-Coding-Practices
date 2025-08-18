551ad96a59f4263039743d2b9f1b898a764fa026
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-18 21:30:15 +0530

    Create 650-string-template.js

---
+++ predictions-output/650-string-template.js
@@ -0,0 +1,7 @@
+// Question: What will be logged?
+const name = "JS";
+const msg = `Hello ${name}!`;
+console.log(msg);
+
+// Answer: Hello JS!
+// Explanation: Template literals allow string interpolation using ${}.
