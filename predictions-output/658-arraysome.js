d032271f0afb45c29f057869b5b6467a9dc516bf
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-19 22:00:17 +0530

    Create 658-arraysome.js

---
+++ predictions-output/658-arraysome.js
@@ -0,0 +1,6 @@
+// Question: What will this log?
+const nums = [1, 3, 5, 6];
+console.log(nums.some(n => n % 2 === 0));
+
+// Answer: true
+// Explanation: some returns true if at least one element passes the test.
