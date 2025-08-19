73f1945592598d91c4d3803d96324e103c90858b
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-19 21:58:22 +0530

    Create 655-arrayfilter.js

---
+++ predictions-output/655-arrayfilter.js
@@ -0,0 +1,6 @@
+// Question: What will this log?
+const nums = [5, 10, 15, 20];
+console.log(nums.filter(n => n > 10));
+
+// Answer: [15, 20]
+// Explanation: filter keeps only elements that satisfy condition.
