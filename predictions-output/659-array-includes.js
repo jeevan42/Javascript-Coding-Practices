ec0e341d04866cb2303fed3608276af47b75ada8
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-19 22:00:46 +0530

    Create 659-array-includes.js

---
+++ predictions-output/659-array-includes.js
@@ -0,0 +1,9 @@
+// Question: What will this log?
+const nums = [1, 2, 3];
+console.log(nums.includes(2));
+console.log(nums.includes(5));
+
+// Answer:
+// true
+// false
+// Explanation: includes checks if an element exists in the array.
