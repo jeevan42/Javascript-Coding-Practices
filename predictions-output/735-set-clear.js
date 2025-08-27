03d38b67e74f2a5cd59dc1de3fa4981734bfbcba
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-27 13:44:34 +0530

    Create 735-set-clear.js

---
+++ predictions-output/735-set-clear.js
@@ -0,0 +1,7 @@
+// Clearing Set
+// Problem: How to remove all values at once?
+// Solution
+const nums = new Set([1, 2, 3]);
+nums.clear();
+console.log(nums); // Set {}
+// Conclusion: Use .clear() to empty a Set completely.
