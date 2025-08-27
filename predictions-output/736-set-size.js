cef03e9ede22557dec8567ce8a9edeed0e89567e
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-27 13:46:17 +0530

    Create 736-set-size.js

---
+++ predictions-output/736-set-size.js
@@ -0,0 +1,6 @@
+// Size of Set
+// Problem: How to find number of unique elements?
+// Solution
+const letters = new Set(["a", "b", "c", "a"]);
+console.log(letters.size); // 3
+// Conclusion: .size gives count of unique elements, unlike array .length.
