511bd2dd4fcf76f9804f51ca933b68280394c173
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-29 13:58:07 +0530

    Create 756-navigator-cookieenabled.js

---
+++ predictions-output/756-navigator-cookieenabled.js
@@ -0,0 +1,5 @@
+// Navigator Cookie Enabled
+// Problem: How to check if cookies are enabled in the browser?
+// Solution
+console.log(navigator.cookieEnabled);
+// Conclusion: navigator.cookieEnabled returns true/false.
