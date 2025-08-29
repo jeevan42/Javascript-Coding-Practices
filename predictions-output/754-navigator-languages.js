294f3cb04493b9b221558063825bde7af865f7e3
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-29 13:56:23 +0530

    Create 754-navigator-languages.js

---
+++ predictions-output/754-navigator-languages.js
@@ -0,0 +1,5 @@
+// Navigator Languages
+// Problem: How to get all supported languages by the browser?
+// Solution
+console.log(navigator.languages);
+// Conclusion: navigator.languages returns an array of preferred languages.
