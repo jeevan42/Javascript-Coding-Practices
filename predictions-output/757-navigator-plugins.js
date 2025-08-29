f3d5984306bff3a814fc58593b5b1791287b62d4
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-29 13:58:44 +0530

    Create 757-navigator-plugins.js

---
+++ predictions-output/757-navigator-plugins.js
@@ -0,0 +1,5 @@
+// Navigator Plugins
+// Problem: How to access installed browser plugins?
+// Solution
+console.log(navigator.plugins);
+// Conclusion: navigator.plugins returns a PluginArray object.
