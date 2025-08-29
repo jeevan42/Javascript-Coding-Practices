0086256b5847c8e7bbd46bbf2c57551a4cc88a65
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-29 13:55:46 +0530

    Create 753-navigator-language.js

---
+++ predictions-output/753-navigator-language.js
@@ -0,0 +1,5 @@
+// Navigator Language
+// Problem: How to find browser's preferred language?
+// Solution
+console.log(navigator.language);
+// Conclusion: navigator.language returns the default browser language.
