8178047cd14873a91037bb810b232c638c8fe4b1
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-29 13:59:28 +0530

    Create 758-navigator-mimetypes.js

---
+++ predictions-output/758-navigator-mimetypes.js
@@ -0,0 +1,5 @@
+// Navigator MimeTypes
+// Problem: How to access supported MIME types?
+// Solution
+console.log(navigator.mimeTypes);
+// Conclusion: navigator.mimeTypes lists supported MIME types.
