73e145d35d520f2ab30f9a9830f609d702253d87
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-30 21:47:38 +0530

    Create 767-navigator-credentials.js

---
+++ predictions-output/767-navigator-credentials.js
@@ -0,0 +1,7 @@
+// Navigator Credentials
+// Problem: How to use Credential Management API?
+// Solution
+if ("credentials" in navigator) {
+  console.log("Credential API supported");
+}
+// Conclusion: navigator.credentials helps with authentication flows.
