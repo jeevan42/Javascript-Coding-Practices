93f1c1455d0a0d14596952e603ee7b7df38cc05c
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-30 21:46:03 +0530

    Add navigator.hardwareConcurrency example

---
+++ predictions-output/764-navigator-hardware-concurrency.js
@@ -0,0 +1,5 @@
+// Navigator Hardware Concurrency
+// Problem: How many logical processors does device have?
+// Solution
+console.log(navigator.hardwareConcurrency);
+// Conclusion: hardwareConcurrency returns CPU thread count.
