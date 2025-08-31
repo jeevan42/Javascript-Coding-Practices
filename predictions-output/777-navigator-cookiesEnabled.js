3f5f9b3ac68d2918d8292a15389d14f1a6b3048a
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-31 22:58:51 +0530

    Create 777-navigator-cookiesEnabled.js

---
+++ predictions-output/777-navigator-cookiesEnabled.js
@@ -0,0 +1,5 @@
+// navigator.cookieEnabled
+// Problem: Check if cookies are enabled in the browser.
+// Solution
+console.log("Cookies Enabled:", navigator.cookieEnabled);
+// Conclusion: navigator.cookieEnabled helps check if site can store cookies.
