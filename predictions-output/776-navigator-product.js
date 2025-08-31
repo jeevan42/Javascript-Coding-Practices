0d6eaa66ff541a8c252beb37660b3e2eb86d01bb
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-31 22:58:23 +0530

    Create 776-navigator-product.js

---
+++ predictions-output/776-navigator-product.js
@@ -0,0 +1,5 @@
+// navigator.product
+// Problem: Find out the browser's rendering engine.
+// Solution
+console.log("Product:", navigator.product);
+// Conclusion: Usually returns "Gecko" even for Chrome (not always reliable).
