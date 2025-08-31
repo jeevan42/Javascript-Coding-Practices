19fe84dc2db41e0ee1ec5b1aec38a48dd354dae9
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-08-31 22:56:34 +0530

    Add navigator.languages logging for preferred languages

---
+++ predictions-output/773-navigator-languages.js
@@ -0,0 +1,5 @@
+// navigator.languages
+// Problem: Some users have multiple preferred languages.
+// Solution
+console.log("Languages:", navigator.languages);
+// Conclusion: navigator.languages gives an array of all preferred languages.
