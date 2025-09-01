e1be382407fd8cc4126a54881f706bf584e6097e
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-09-01 22:36:42 +0530

    Create 785-navigator-deviceMemory.js

---
+++ predictions-output/785-navigator-deviceMemory.js
@@ -0,0 +1,5 @@
+// navigator.deviceMemory
+// Problem: Check device memory to optimize app performance.
+// Solution
+console.log("Device Memory (GB):", navigator.deviceMemory);
+// Conclusion: Helps design memory-efficient applications.
