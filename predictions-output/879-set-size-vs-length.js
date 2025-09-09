b513ac1bc006faffaddf0541933c5708292bc23f
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-09-10 00:23:07 +0530

    Create 879-set-size-vs-length.js

---
+++ predictions-output/879-set-size-vs-length.js
@@ -0,0 +1,6 @@
+// Concept: Difference between size and length
+// Problem: Why Set doesn’t use length?
+// Solution:
+const sSize=new Set([1,2]);
+console.log(sSize.size); // 2
+// Conclusion: Sets use .size, arrays use .length.
