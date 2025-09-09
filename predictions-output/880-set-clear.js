58020cedc8cd1da254bf712a510ed5d34f73e8c6
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-09-10 00:23:39 +0530

    Create 880-set-clear.js

---
+++ predictions-output/880-set-clear.js
@@ -0,0 +1,7 @@
+// Concept: Clear all elements in Set
+// Problem: How to empty a Set?
+// Solution:
+const sClear=new Set([1,2,3]);
+sClear.clear();
+console.log(sClear); // Set {}
+// Conclusion: .clear() empties the Set in place.
