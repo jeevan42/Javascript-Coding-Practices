a9c3d30e4587cb69d3cef29917a954e977d6185e
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-09-12 00:34:41 +0530

    Add Set iteration example in 900-set-iteration.js

---
+++ predictions-output/900-set-iteration.js
@@ -0,0 +1,6 @@
+// Iterating Set
+// Problem: Iterate through set values.
+// Solution:
+const s = new Set([1,2,3]);
+for (let val of s) console.log(val);
+// Conclusion: Sets are iterable with for...of.
