7fa16fe72b6aeab51a74b8d57c9b729f81552da3
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-09-13 00:04:39 +0530

    Create 903-set-to-string.js

---
+++ predictions-output/903-set-to-string.js
@@ -0,0 +1,6 @@
+// Set to String
+// Problem: Convert set of chars to string.
+// Solution:
+const s = new Set(['h','e','l','l','o']);
+console.log([...s].join(""));
+// Conclusion: Spread + join makes string.
