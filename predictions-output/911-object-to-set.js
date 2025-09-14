9f7177deafceeb0653e83042fcf86175ea04b685
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-09-14 11:04:18 +0530

    Create 911-object-to-set.js

---
+++ predictions-output/911-object-to-set.js
@@ -0,0 +1,7 @@
+// Object to Set
+// Problem: Extract keys of object as Set.
+// Solution:
+const obj = {a:1, b:2, c:3};
+const s = new Set(Object.keys(obj));
+console.log(s);
+// Conclusion: Keys can be turned into Set.
